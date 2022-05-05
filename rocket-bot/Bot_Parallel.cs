a49d6523bc96c5755c64ca802b8b50c9edee2e40
Alex <iseldomused@gmail.com>
2022-05-05 17:02:29 +0300

    solve bot parallel

--- rocket-bot/Bot_Parallel.cs
+++ rocket-bot/Bot_Parallel.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace rocket_bot
 {
@@ -8,10 +8,42 @@ namespace rocket_bot
     {
         public Rocket GetNextMove(Rocket rocket)
         {
-            // TODO: распараллелить запуск SearchBestMove
-            var bestMove = SearchBestMove(rocket, new Random(random.Next()), iterationsCount);
-            var newRocket = rocket.Move(bestMove.Item1, level);
-            return newRocket;
+            return rocket.Move(FindBestMove(rocket), level);
+        }
+
+        private Turn FindBestMove(Rocket rocket)
+        {
+            var moves = 
+                DistributeNumber(iterationsCount, threadsCount).AsParallel()
+                .Select(cnt => SearchBestMove(rocket, new Random(NextRandom()), cnt))
+                .ToList();
+            var bestScore = moves.Max(m => m.Item2);
+            return moves.First(m => m.Item2 == bestScore).Item1;
+        }
+
+        private static IEnumerable<int> DistributeNumber(int number, int chunksNumber)
+        {
+            if (chunksNumber == 0)
+            {
+                yield return 0;
+                yield break;
+            }
+
+            var rest = number % chunksNumber;
+            var chunk = number / chunksNumber;
+            for (int i = 0; i < chunksNumber; i++)
+            {
+                yield return rest > 0 ? chunk + 1 : chunk;
+                rest--;
+            }
+        }
+
+        private int NextRandom()
+        {
+            lock (random)
+            {
+                return random.Next();
+            }
         }
     }
 }
