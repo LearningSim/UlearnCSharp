0e7499ff67d311f118e634c71d13c6be656757a5
Alex <iseldomused@gmail.com>
2022-09-10 14:30:59 +0300

    solve benchmark subtask

--- StructBenchmarking/BenchmarkTask.cs
+++ StructBenchmarking/BenchmarkTask.cs
@@ -1,6 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
+using System.Text;
 using NUnit.Framework;
 
 namespace StructBenchmarking
@@ -12,9 +12,17 @@ namespace StructBenchmarking
             GC.Collect();                   // Эти две строчки нужны, чтобы уменьшить вероятность того,
             GC.WaitForPendingFinalizers();  // что Garbadge Collector вызовется в середине измерений
                                             // и как-то повлияет на них.
-               
-			throw new NotImplementedException();
-		}
+                                            
+			task.Run();
+            var timer = new Stopwatch();
+            timer.Start();
+            for (int i = 0; i < repetitionCount; i++)
+            {
+                task.Run();
+            }
+            timer.Stop();
+            return (double)timer.ElapsedMilliseconds / repetitionCount;
+        }
 	}
 
     [TestFixture]
@@ -23,7 +31,32 @@ namespace StructBenchmarking
         [Test]
         public void StringConstructorFasterThanStringBuilder()
         {
-            throw new NotImplementedException();
+            var bench = new Benchmark();
+            var constr = bench.MeasureDurationInMs(new StringConstructorTask(),  100);
+            var builder = bench.MeasureDurationInMs(new StringBuilderTask(), 100);
+            Assert.Less(constr, builder);
+        }
+    }
+
+    public class StringBuilderTask : ITask
+    {
+        public void Run()
+        {
+            var builder = new StringBuilder();
+            for (int i = 0; i < 1000000; i++)
+            {
+                builder.Append('a');
+            }
+
+            var str = builder.ToString();
+        }
+    }
+
+    public class StringConstructorTask : ITask
+    {
+        public void Run()
+        {
+            var str = new string('a', 1000000);
         }
     }
 }
