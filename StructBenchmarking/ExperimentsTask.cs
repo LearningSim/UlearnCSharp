45db12caa3479fd31b014ac4cf10d3648f87c676
Alex <iseldomused@gmail.com>
2022-09-16 19:23:34 +0300

    solve experiments subtask

--- StructBenchmarking/ExperimentsTask.cs
+++ StructBenchmarking/ExperimentsTask.cs
@@ -1,39 +1,46 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace StructBenchmarking
 {
     public class Experiments
     {
-        public static ChartData BuildChartDataForArrayCreation(
-            IBenchmark benchmark, int repetitionsCount)
+        public static ChartData BuildChartDataForArrayCreation(IBenchmark benchmark, int repetitionsCount)
         {
-            var classesTimes = new List<ExperimentResult>();
-            var structuresTimes = new List<ExperimentResult>();
-            
-            //...
-
+            ITask CreateClassTask(int size) => new ClassArrayCreationTask(size);
+            ITask CreateStructTask(int size) => new StructArrayCreationTask(size);
             return new ChartData
             {
                 Title = "Create array",
-                ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
+                ClassPoints = MakeExperiments(benchmark, repetitionsCount, CreateClassTask),
+                StructPoints = MakeExperiments(benchmark, repetitionsCount, CreateStructTask),
             };
         }
 
-        public static ChartData BuildChartDataForMethodCall(
-            IBenchmark benchmark, int repetitionsCount)
+        public static ChartData BuildChartDataForMethodCall(IBenchmark benchmark, int repetitionsCount)
         {
-            var classesTimes = new List<ExperimentResult>();
-            var structuresTimes = new List<ExperimentResult>();
-            
-            //...
-
+            ITask CreateClassTask(int size) => new MethodCallWithClassArgumentTask(size);
+            ITask CreateStructTask(int size) => new MethodCallWithStructArgumentTask(size);
             return new ChartData
             {
                 Title = "Call method with argument",
-                ClassPoints = classesTimes,
-                StructPoints = structuresTimes,
+                ClassPoints = MakeExperiments(benchmark, repetitionsCount, CreateClassTask),
+                StructPoints = MakeExperiments(benchmark, repetitionsCount, CreateStructTask),
             };
+        }
+
+        private static List<ExperimentResult> MakeExperiments(IBenchmark benchmark, int repetitionsCount, Func<int, ITask> createTask)
+        {
+            return Constants.FieldCounts
+                .Select(size => MakeExperiment(benchmark, repetitionsCount, size, createTask))
+                .ToList();
+        }
+
+        private static ExperimentResult MakeExperiment(IBenchmark benchmark, int repetitionsCount, int fieldCount, Func<int, ITask> createTask)
+        {
+            var time = benchmark.MeasureDurationInMs(createTask(fieldCount), repetitionsCount);
+            return new ExperimentResult(fieldCount, time);
         }
     }
 }
