720ab77848546fe52ae23f2ae862db8f56cc3800
Alex <iseldomused@gmail.com>
2022-09-17 13:59:40 +0300

    solve Documentation task

--- Documentation/Specifier.cs
+++ Documentation/Specifier.cs
@@ -6,39 +6,59 @@ namespace Documentation
 {
     public class Specifier<T> : ISpecifier
     {
-        public string GetApiDescription()
+        public string GetApiDescription() => 
+            typeof(T).GetCustomAttribute<ApiDescriptionAttribute>()?.Description;
+
+        public string[] GetApiMethodNames() =>
+            typeof(T).GetMethods()
+                .Where(m => m.GetCustomAttribute<ApiMethodAttribute>() != null)
+                .Select(m => m.Name).ToArray();
+
+        public string GetApiMethodDescription(string methodName) =>
+            typeof(T).GetMethod(methodName)?
+                .GetCustomAttribute<ApiDescriptionAttribute>()?.Description;
+
+        public string[] GetApiMethodParamNames(string methodName) =>
+            typeof(T).GetMethod(methodName)?.GetParameters()
+                .Select(p => p.Name).ToArray();
+
+        public string GetApiMethodParamDescription(string methodName, string paramName) => 
+            GetMethodParam(methodName, paramName)?.GetCustomAttribute<ApiDescriptionAttribute>()?.Description;
+
+        public ApiParamDescription GetApiMethodParamFullDescription(string methodName, string paramName) => 
+            GetApiMethodParamFullDescription(GetMethodParam(methodName, paramName), methodName, paramName);
+
+        private ApiParamDescription GetApiMethodParamFullDescription(ParameterInfo param, string methodName, string paramName)
         {
-            throw new NotImplementedException();
+            var intValidation = param?.GetCustomAttribute<ApiIntValidationAttribute>();
+            return new ApiParamDescription
+            {
+                MinValue = intValidation?.MinValue,
+                MaxValue = intValidation?.MaxValue,
+                Required = param?.GetCustomAttribute<ApiRequiredAttribute>()?.Required == true,
+                ParamDescription = new CommonDescription(paramName, GetApiMethodParamDescription(methodName, paramName)),
+            };
         }
 
-        public string[] GetApiMethodNames()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetApiMethodDescription(string methodName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string[] GetApiMethodParamNames(string methodName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string GetApiMethodParamDescription(string methodName, string paramName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ApiParamDescription GetApiMethodParamFullDescription(string methodName, string paramName)
-        {
-            throw new NotImplementedException();
-        }
+        private ParameterInfo GetMethodParam(string methodName, string paramName) =>
+            typeof(T).GetMethod(methodName)?
+                .GetParameters().FirstOrDefault(p => p.Name == paramName);
 
         public ApiMethodDescription GetApiMethodFullDescription(string methodName)
         {
-            throw new NotImplementedException();
+            var method = typeof(T).GetMethod(methodName);
+            var isApiMethod = method?.GetCustomAttribute<ApiMethodAttribute>() != null;
+            if (!isApiMethod) return null;
+
+            return new ApiMethodDescription
+            {
+                MethodDescription = new CommonDescription(methodName, GetApiMethodDescription(methodName)),
+                ParamDescriptions = GetApiMethodParamNames(methodName)
+                    .Select(p => GetApiMethodParamFullDescription(methodName, p)).ToArray(),
+                ReturnDescription = method.ReturnType != typeof(void) ? 
+                    GetApiMethodParamFullDescription(method.ReturnParameter, methodName, null) :
+                    null
+            };
         }
     }
 }
