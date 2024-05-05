0b5cfd31bcc5cf98665895f90cdde5cdfeef633c
Simplifier <iseldomused@gmail.com>
2024-05-05 22:59:07 +0400

    solve autocomplete - left border

--- autocomplete/LeftBorderTask.cs
+++ autocomplete/LeftBorderTask.cs
@@ -9,27 +9,24 @@ namespace Autocomplete;
 // с учетом регистра, без учета, зависеть от кодировки и т.п.
 // В файле словаря все слова отсортированы методом StringComparison.InvariantCultureIgnoreCase.
 // Во всех функциях сравнения строк в C# можно передать способ сравнения.
-public class LeftBorderTask
+public static class LeftBorderTask
 {
-	/// <returns>
-	/// Возвращает индекс левой границы.
-	/// То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
-	/// Если такой нет, то возвращает -1
-	/// </returns>
-	/// <remarks>
-	/// Функция должна быть рекурсивной
-	/// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
-	/// </remarks>
-	public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
-	{
-		// IReadOnlyList похож на List, но у него нет методов модификации списка.
-		// Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-		for (int i = 0; i < phrases.Count; i++)
-		{
-			if (string.Compare(prefix, phrases[i], StringComparison.InvariantCultureIgnoreCase) < 0
-			    || phrases[i].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
-				return i - 1;
-		}
-		return phrases.Count-1;
-	}
+    /// <returns>
+    /// Возвращает индекс левой границы.
+    /// То есть индекс максимальной фразы, которая не начинается с prefix и меньшая prefix.
+    /// Если такой нет, то возвращает -1
+    /// </returns>
+    /// <remarks>
+    /// Функция должна быть рекурсивной
+    /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+    /// </remarks>
+    public static int GetLeftBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    {
+        if (left == right - 1) return left;
+        var m = (int)(((long)left + right) / 2);
+        const StringComparison comparison = StringComparison.InvariantCultureIgnoreCase;
+        return string.Compare(prefix, phrases[m], comparison) > 0
+            ? GetLeftBorderIndex(phrases, prefix, m, right)
+            : GetLeftBorderIndex(phrases, prefix, left, m);
+    }
 }
