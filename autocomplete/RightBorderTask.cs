a7f29f218455189b0e2723074b808e7bc4639733
Simplifier <iseldomused@gmail.com>
2024-05-08 00:26:27 +0400

    solve autocomplete - right border

--- autocomplete/RightBorderTask.cs
+++ autocomplete/RightBorderTask.cs
@@ -4,27 +4,34 @@ using System.Linq;
 
 namespace Autocomplete;
 
-public class RightBorderTask
+public static class RightBorderTask
 {
-	/// <returns>
-	/// Возвращает индекс правой границы. 
-	/// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
-	/// Если такого нет, то возвращает items.Length
-	/// </returns>
-	/// <remarks>
-	/// Функция должна быть НЕ рекурсивной
-	/// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
-	/// </remarks>
-	public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
-	{
-		// IReadOnlyList похож на List, но у него нет методов модификации списка.
-		// Этот код решает задачу, но слишком неэффективно. Замените его на бинарный поиск!
-		for (int i = phrases.Count-1; i >= 0; i--)
-		{
-			if (string.Compare(prefix, phrases[i], StringComparison.InvariantCultureIgnoreCase) >= 0 
-			    || phrases[i].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase))
-				return i + 1;
-		}
-		return 0;
-	}
+    /// <returns>
+    /// Возвращает индекс правой границы. 
+    /// То есть индекс минимального элемента, который не начинается с prefix и большего prefix.
+    /// Если такого нет, то возвращает items.Length
+    /// </returns>
+    /// <remarks>
+    /// Функция должна быть НЕ рекурсивной
+    /// и работать за O(log(items.Length)*L), где L — ограничение сверху на длину фразы
+    /// </remarks>
+    public static int GetRightBorderIndex(IReadOnlyList<string> phrases, string prefix, int left, int right)
+    {
+        while (true)
+        {
+            if (left == right - 1) return right;
+            var m = left + (right - left) / 2;
+            const StringComparison comparison = StringComparison.InvariantCultureIgnoreCase;
+            var prefixLessMiddle = string.Compare(prefix, phrases[m], comparison) < 0 &&
+                                   !phrases[m].StartsWith(prefix, StringComparison.InvariantCultureIgnoreCase);
+            if (prefixLessMiddle)
+            {
+                right = m;
+            }
+            else
+            {
+                left = m;
+            }
+        }
+    }
 }
---
+++ autocomplete/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Autocomplete;
+
+[TestFixture]
+public class Tests
+{
+    [TestCase(new[] { "a", "a" }, "a", 2)]
+    [TestCase(new[] { "a", "b" }, "a", 1)]
+    [TestCase(new[] { "b", "b" }, "a", 0)]
+    [TestCase(new[] { "a", "b", "b", "b" }, "a", 1)]
+    [TestCase(new[] { "ab", "b", "b", "b" }, "a", 1)]
+    [TestCase(new[] { "ab", "b", "b", "b" }, "ab", 1)]
+    [TestCase(new string[] { }, "a", 0)]
+    [TestCase(new[] { "a", "ab", "abc" }, "aa", 1)]
+    [TestCase(new[] { "ab", "ab", "ab", "ab" }, "a", 4)]
+    [TestCase(new[] { "ab", "ab", "ab", "ab" }, "aa", 0)]
+    public void TestRightBorder(string[] phrases, string prefix, int expectedResult)
+    {
+        var actual = RightBorderTask.GetRightBorderIndex(phrases, prefix, -1, phrases.Length);
+        Assert.AreEqual(expectedResult, actual);
+    }
+}
