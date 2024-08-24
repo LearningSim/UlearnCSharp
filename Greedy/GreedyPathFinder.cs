72d89c6902d7d5aa4f21ca6b948991bef4c43d00
Simplifier <iseldomused@gmail.com>
2024-08-24 20:27:16 +0400

    solve greedy.GreedyPathFinder

--- Greedy/GreedyPathFinder.cs
+++ Greedy/GreedyPathFinder.cs
@@ -1,12 +1,35 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Greedy.Architecture;
 
 namespace Greedy;
 
 public class GreedyPathFinder : IPathFinder
 {
-	public List<Point> FindPathToCompleteGoal(State state)
-	{
-		return new List<Point>();
-	}
+    public List<Point> FindPathToCompleteGoal(State state)
+    {
+        var totalPath = new List<Point>();
+        var chests = state.Chests.ToHashSet();
+        var pos = state.Position;
+        var energy = state.Energy;
+        for (int i = 0; i < state.Goal; i++)
+        {
+            var path = GetPathToFirstChest(state, pos, chests);
+            energy -= path?.Cost ?? 0;
+            if (path == null || energy < 0)
+            {
+                return new List<Point>();
+            }
+            
+            totalPath.AddRange(path.Path.Skip(1));
+            pos = path.Path.Last();
+            chests.Remove(pos);
+        }
+
+        return totalPath;
+    }
+
+    private PathWithCost? GetPathToFirstChest(State state, Point start, IEnumerable<Point> chests) =>
+        new DijkstraPathFinder().GetPathsByDijkstra(state, start, chests)
+            .FirstOrDefault();
 }
